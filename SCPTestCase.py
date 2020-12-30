26b3dc2b6ece728df01730c61a2ca2d915245a59
Stefan Bonney <stefan.bonney@gmail.com>
2020-12-30 12:00:34 +0200

    Update SCPTestCase.py
    changed comments

--- SCPTestCase.py
+++ SCPTestCase.py
@@ -7,8 +7,8 @@ import paramiko
 
 
 # DESCRIPTION
-# Script that sends a file via scp to remote host
-# variables either defined in the source code or defined when prompted for user input, 
+# Script that sends a file via scp to remote host, then checks that the wording of the transferred file matches the original.
+# Variables either defined in the source code or defined when prompted for user input, 
 # Can be used f.ex. for sending configuration files or script files, and checking for the file integrity once transferred
 # COMMAND LINE OPTIONS
 # use [ -v ] to have printed the original and searched transferred content upon making comparison  
